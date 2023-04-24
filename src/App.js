f2749d0a274be1791967173ec06291d1ceb75bc5
Nagesh B C <123981161+NageshSiddu296@users.noreply.github.com>
2023-04-24 14:34:03 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="App">
+        <h1 className="heading">Super Over League</h1>
+        <div className="pics">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+            alt=""
+          />
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
